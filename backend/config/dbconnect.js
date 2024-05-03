c0c22a0983b1730893598c565177e2ce135ddfb7
tranvanthinh <badagor999@gmail.com>
2024-05-04 02:05:07 +0700

    build layout loggin

--- backend/config/dbconnect.js
+++ backend/config/dbconnect.js
@@ -1,20 +1,20 @@
-const { mongoose } = require('mongoose')
+const { mongoose } = require("mongoose");
 
-
-const dbConnect = async() => {
-    try{
-        const con = await mongoose.connect(process.env.URL_DB)
-        if (con.connection.readyState === 1) {
-            console.log('DB connection is successfully')
-        } else {
-            console.log('DB connecting')
-        }
-    } catch(e) {
-        console.log('DB connect is failed')
+const dbConnect = async () => {
+  try {
+    const con = await mongoose.connect(
+      "mongodb+srv://cinema:vinh051003@cinama.vacpitm.mongodb.net/?retryWrites=true&w=majority&appName=cinama"
+    );
+    if (con.connection.readyState === 1) {
+      console.log("DB connection is successfully");
+    } else {
+      console.log("DB connecting");
     }
-}
-
+  } catch (e) {
+    console.log("DB connect is failed");
+  }
+};
 
 module.exports = {
-    dbConnect
-}
+  dbConnect,
+};
--- backend/routers/movie.js
+++ backend/routers/movie.js
@@ -1,11 +1,15 @@
-const router = require('express').Router()
-const { createMovie, getListMovie, updateStatusMovie, deleteMovie} = require('../controllers/movie')
-const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
+const router = require("express").Router();
+const {
+  createMovie,
+  getListMovie,
+  updateStatusMovie,
+  deleteMovie,
+} = require("../controllers/movie");
+const { verifyAccessToken, isAdmin } = require("../middlewares/verifyToken");
 
-router.post('/create', createMovie)
-router.get('/list/:type', getListMovie)
-router.put('/status', updateStatusMovie)
-router.delete('/:mid', deleteMovie)
+router.post("/create", createMovie);
+router.get("/list/:type", getListMovie);
+router.put("/status", updateStatusMovie);
+router.delete("/:mid", deleteMovie);
 
-
-module.exports = router
+module.exports = router;
